028a623aafb12bfa216645da1fa5c4b930c052b6
sujj <brightpegion@gmail.com>
2016-08-21 18:07:52 +0800

    v2

--- L3Core/src/com/L3/Engine/CoreEngine.java
+++ L3Core/src/com/L3/Engine/CoreEngine.java
@@ -5,6 +5,8 @@ public class CoreEngine {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Join the code stream test1");
+		
+		System.out.println("Join the new code test2");;
 	}
 
 }
